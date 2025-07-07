cf76ea1829d94e407bf4203cffb635b80fee6c0f
anyshy <wa.zy@163.com>
2025-07-07 14:38:23 +0800

    refactor(framework): 修改 BlockTransactionPrinter 命令行参数名称
    - 将 --fmt 参数重命名为 --out_fmt，以更清晰地表示输出格式
    - 更新了相应的帮助信息和参数解析逻辑

--- framework/src/main/java/org/tron/program/BlockTransactionPrinter.java
+++ framework/src/main/java/org/tron/program/BlockTransactionPrinter.java
@@ -156,14 +156,14 @@ public class BlockTransactionPrinter {
       System.out.println("Options:");
       System.out.println("  -c <config_file>: Specify a custom configuration file");
       System.out.println("  -d <data_dir>: Specify a custom data directory");
-      System.out.println("  --fmt <format>: Output format (json|protobuf|both), default: json");
+      System.out.println("  --out_fmt <format>: Output format (json|protobuf|both), default: json");
       return;
     }
 
     // Parse output format option
     String outputFormat = "json"; // default
     for (int i = 0; i < args.length - 1; i++) {
-      if ("--fmt".equals(args[i])) {
+      if ("--out_fmt".equals(args[i])) {
         outputFormat = args[i + 1].toLowerCase();
         break;
       }
